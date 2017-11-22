e99dd44998bf8e07b1fe41782e8fd13af9f39cb6
Saurabh <saurabh.rawat7@yahoo.com>
2017-11-22 14:38:57 +0530

    New product entity
    Generated product entity via jhipster cmd line options:
    "jhipster entity product"

---
+++ src/main/java/com/saurabh/store/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.saurabh.store.repository;
+
+import com.saurabh.store.domain.Product;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Product entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
